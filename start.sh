1acb0baf733335eb2bd9bef84863c6c76f5803e7
Clément Planque <clementplanque@gmail.com>
2024-08-31 13:37:25 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+cd /root/public-drive
+
 npm i
-node .\server.js
+node server.js
